6bbdb36dfb1258931b7f77a3692d375a587cb621
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-12-01 10:26:38 +0000

    example of click_link added

---
+++ check_click.rb
@@ -0,0 +1,12 @@
+require 'capybara/dsl'
+require 'capybara/poltergeist'
+
+Capybara.default_driver = :poltergeist
+
+session = Capybara::Session.new(:poltergeist)
+session.visit "http://crguezl.github.io/selenium-examples/index.html"
+
+session.click_link "example1"
+
+session.save_page('example1.html')
+
